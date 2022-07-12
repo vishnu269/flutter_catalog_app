ad139686a733496068d657f8444b2d6856e75d72
FlutLab.io <>
2022-07-12 12:06:13 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
+      darkTheme: ThemeData(brightness: Brightness.dark),
     );
   }
 }
