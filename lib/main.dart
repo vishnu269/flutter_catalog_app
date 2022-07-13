a77a9e5a5bc75bd56a68271939f1258a886643d7
FlutLab.io <>
2022-07-13 08:24:09 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: "/",
       routes: {
+        "/": (context) => LoginPage(),
         MyRoutes.loginRoute: (context) => LoginPage(),
         MyRoutes.homeRoute: (context) => HomePage(),
       },
