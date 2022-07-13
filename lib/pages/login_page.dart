c99f18eab893f4a679a788a40bab5755b25593f4
FlutLab.io <>
2022-07-13 04:26:38 +0000

    Autosave

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -5,12 +7,18 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(fontSize: 20.0, color: Colors.green, fontWeight: FontWeight.bold),
-        ),
+      child: Column(
+        children: [
+          Image.asset("assets/images/login_image.png"),
+        ],
       ),
     );
   }
 }
+
+// Center(
+//         child: Text(
+//           "Login Page",
+//           style: TextStyle(fontSize: 20.0, color: Colors.green, fontWeight: FontWeight.bold),
+//         ),
+//       ),
