8b355c1500a56111fab70a4f63e4e6f494ad7f92
FlutLab.io <>
2022-07-13 05:13:03 +0000

    Autosave

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,10 +3,10 @@ import "dart:ui";
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Material(
+      color: Colors.white,
       child: Column(
         children: [
           Image.asset(
