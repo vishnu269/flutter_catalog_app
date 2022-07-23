f5455643acae60fc3d40d932a4def672990e670f
FlutLab.io <>
2022-07-23 23:22:12 +0000

    Autosave

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,15 +8,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Center(
-        child: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0.0,
-          iconTheme: IconThemeData(color: Colors.black),
-          title: Text(
-            "Catalog App",
-            style: TextStyle(color: Colors.black),
-          ),
+      appBar: AppBar(
+        title: Text(
+          "Catalog App",
         ),
       ),
       body: Center(
